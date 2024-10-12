1147a1318f661872992939c8fcc3d5e7148e5e60
Azryy <doradokurt02@gmail.com>
2024-10-12 18:03:05 +0800

    api's in the db successfully working

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -28,8 +28,8 @@ export const register = async (req, res) => {
 
         })
         return res.status(201).json({
-            message:"Account created successfully",
-            success:true
+            message: "Account created successfully",
+            success: true
         })
     } catch (error) {
         console.log(error);
@@ -45,7 +45,7 @@ export const login = async (req, res) => {
                 success: false
             })
         }
-        const user = await User.findOne({ email })
+        let user = await User.findOne({ email })
         if (!user) {
             return res.status(400).json({
                 message: "Incorrect email or password",
@@ -73,12 +73,12 @@ export const login = async (req, res) => {
         const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' })
 
         user = {
-            _id : user._id,
-            fullname : user.fullname,
-            email : user.email,
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
             phoneNumber: user.phoneNumber,
-            role:user.role,
-            profile:user.profile
+            role: user.role,
+            profile: user.profile
         }
 
         return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
@@ -92,61 +92,64 @@ export const login = async (req, res) => {
     }
 }
 
-export const logout = async (req,res) => {
+export const logout = async (req, res) => {
     try {
-        return res.status(200).cookie("token", "", {maxAge:0}).json({
-            message:"Logged out successfully",
-            success:true
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logged out successfully",
+            success: true
         })
     } catch (error) {
         console.log(error);
     }
 }
 
-export const updateProfile = async (req,res) => {
+export const updateProfile = async (req, res) => {
     try {
-        const {fullname,email, phoneNumber, bio, skills} = req.body;
+        const { fullname, email, phoneNumber, bio, skills } = req.body;
         const file = req.file;
-        if (!fullname || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({
-                message: "Something is missing",
-                success: false
-            });
-        };
+        
 
+        let skillsArray;
+        if(skills){
+            skillsArray = skills.split(",");
+        }
 
-
-        const skillsArray = skills.split(",");
+        
         const userId = req.id; // middleware authentication
         let user = await User.findById(userId);
         if (!user) {
             return res.status(400).json({
-                message:"User not found",
-                success:false
+                message: "User not found",
+                success: false
             })
         }
         //updating data
-        user.fullname = fullname,
-        user.email = email,
-        user.phoneNumber = phoneNumber,
-        user.profile.bio = bio,
-        user.profile.skills = skillsArray
+        if(fullname)  user.fullname = fullname
+        if(email)  user.email = email
+        if(phoneNumber)  user.phoneNumber = phoneNumber
+        if(bio) user.profile.bio = bio
+        if(skills)  user.profile.skills = skillsArray
+       
+
+       
+           
+            
 
         //saving update
         await user.save();
 
         user = {
-            _id : user._id,
-            fullname : user.fullname,
-            email : user.email,
+            _id: user._id,
+            fullname: user.fullname,
+            email: user.email,
             phoneNumber: user.phoneNumber,
-            role:user.role,
-            profile:user.profile
+            role: user.role,
+            profile: user.profile
         }
         return res.status(200).json({
-            message:"Profile updated successfully",
+            message: "Profile updated successfully",
             user,
-            success:true
+            success: true
         })
     } catch (error) {
         console.log(error);
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+
 
 dotenv.config({});
 
@@ -23,8 +25,20 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 
-//Port number testing
+
 const PORT = process.env.PORT || 3000;
+
+//api's
+app.use("/api/v1/user", userRoute);
+
+"http://localhost:8000/api/v1/user/register"
+"http://localhost:8000/api/v1/user/login"
+"http://localhost:8000/api/v1/user/profile/update"
+
+
+
+
+//Port number testing
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
