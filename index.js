de15fdb1dc8c4d8b1456292eeae23a50f110daf1
Azryy <doradokurt02@gmail.com>
2024-12-06 19:46:07 +0800

    Fix the cors

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOptions = {
-    origin:'https://gradconnect.site',
+   origin: ['https://gradconnect.site', 'http://localhost:5173'],
     credentials:true
 }
 app.use(cors(corsOptions));
